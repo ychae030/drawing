d13fe08c4453946d542de7c7122c3f89298a42ae
yuchaewon <yucha1997@etarae.com>
2025-10-02 10:26:41 +0900

    init

---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
